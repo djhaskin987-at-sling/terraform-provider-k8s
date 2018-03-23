c99219c0a94220da00ed8fa183c987b37caf2396
Daniel Jay Haskin <daniel.haskin@sling.com>
2018-03-23 11:32:07 -0600

    Add libraries necessary for minifying JSON in the state

--- main.go
+++ main.go
@@ -4,12 +4,15 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"github.com/ghodss/yaml"
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/hashicorp/terraform/plugin"
 	"github.com/hashicorp/terraform/terraform"
-	"gopkg.in/yaml.v2"
+	"github.com/tdewolff/minify"
+	minjson "github.com/tdewolff/minify/json"
 	"log"
 	"os/exec"
+	"regexp"
 	"strings"
 )
 
@@ -25,17 +28,25 @@ func main() {
 	})
 }
 
+// This converts everything to minified JSON.  This is convenient because it
+// makes the state smaller, which helps in case you are using the consul
+// backend to store your data.  It also helps because it avoids subtle edge
+// case bugs and formatting issues associated with slurping yaml in, then just
+// spitting it out.  I have seen it cause problems when multi-line strings
+// enter the picture.  Far better to normalize to json
 func normalizeInput(input string) (string, error) {
-	var m map[interface{}]interface{}
-	err := yaml.Unmarshal([]byte(input), &m)
+	j, err := yaml.YAMLToJSON([]byte(input))
 	if err != nil {
 		return "", err
 	}
-	normalized, err := yaml.Marshal(&m)
-	if err != nil {
+	var b strings.Builder
+	r := strings.NewReader(string(j))
+	m := minify.New()
+	m.AddFuncRegexp(regexp.MustCompile("[/+]json$"), minjson.Minify)
+	if err := m.Minify("application/json", &b, r); err != nil {
 		return "", err
 	}
-	return string(normalized), nil
+	return string(b.String()), nil
 }
 
 func attemptNormalizeInput(input string) string {
